6dce2bb599fe9400d97a30edc160fa9b489f1126
Franco Ortega <francopdx@gmail.com>
2021-03-30 11:43:50 -0700

    update grid items with a longer row and a longer column

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -6,6 +6,17 @@ const Grid = props => {
     return (
         <div className={styles.Grid}>
             <h2>Grid</h2>
+            <div className={styles.GridContainer}>
+                <div className={styles.GridItem}>1</div>
+                <div className={styles.GridItem}>2</div>
+                <div className={`${styles.GridItem} ${styles.GridItem3}`}>3</div>
+                <div className={styles.GridItem}>4</div>
+                <div className={styles.GridItem}>5</div>
+                <div className={styles.GridItem}>6</div>
+                <div className={styles.GridItem}>7</div>
+                <div className={styles.GridItem}>8</div>
+                <div className={`${styles.GridItem} ${styles.GridItem9}`}>9</div>
+            </div>
         </div>
     );
 };
