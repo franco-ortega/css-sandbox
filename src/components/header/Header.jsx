8e2b7c6fc6e314ac93c7a5a9655879e69daa6c48
Franco Ortega <francopdx@gmail.com>
2021-08-26 16:07:51 -0700

    add Nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import Nav from '../nav/Nav';
 import styles from './Header.module.css';
 
 const Header = props => {
-    return (
-        <div className={styles.Header}>
-            <section>
-            <h1>CSS Sandbox</h1>
-            </section>
-            <section>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/grid'>Grid</Link></li>
-                    <li><Link to='/gridmap6x5'>Grid Map (6 x 5)</Link></li>
-                    <li><Link to='/gridmap9x5'>Grid Map (9 x 5)</Link></li>
-                    <li><Link to='/shapes'>Shapes</Link></li>
-                    <li><Link to='/circlefloating'>Circle</Link></li>
-                </ul>
-            </section>
-            
-        </div>
-    );
-};
-
-Header.propTypes = {
-
+  return (
+    <div className={styles.Header}>
+      <h1>CSS Sandbox</h1>
+      <Nav />
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Nav.module.css';
+
+const Nav = () => {
+  return (
+    <ul className={styles.Nav}>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/grid'>Grid</Link></li>
+      <li><Link to='/gridmap6x5'>Grid Map (6 x 5)</Link></li>
+      <li><Link to='/gridmap9x5'>Grid Map (9 x 5)</Link></li>
+      <li><Link to='/shapes'>Shapes</Link></li>
+      <li><Link to='/circlefloating'>Circle</Link></li>
+    </ul>
+  );
+};
+
+export default Nav;
